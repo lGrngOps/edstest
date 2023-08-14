68bc5ba56df53189da569b906a2d3267c70d1fea
Railson Magalhaes <railson.sistemas@gmail.com>
2023-08-14 10:18:38 -0300

    Iniciando Teste Full Stack by EDS

---
+++ src/main/java/com/eds/edstest/EdstestApplication.java
@@ -0,0 +1,13 @@
+package com.eds.edstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdstestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eds/edstest/EdstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eds.edstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdstestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
