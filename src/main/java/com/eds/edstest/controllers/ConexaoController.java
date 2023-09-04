9e553045a45545e8513a68c53bd11c08b159f9f6
Railson Magalhaes <railson.sistemas@gmail.com>
2023-09-04 16:44:13 -0300

    Correção de Bugs

--- src/main/java/com/eds/edstest/controllers/ConexaoController.java
+++ src/main/java/com/eds/edstest/controllers/ConexaoController.java
@@ -11,10 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.Instant;
@@ -53,7 +50,7 @@ public class ConexaoController {
     }
 
     @GetMapping("/apagar/{id}")
-    public String apagarVeiculo(@PathVariable("id") long id, Model model) {
+    public String apagarVeiculo(@PathVariable("id") long id) {
         VeiculoDTO veiculoDTO = veiculoService.findVeiculoById(id);
         veiculoService.deleteById(id);
         return "redirect:/listar";
@@ -62,14 +59,14 @@ public class ConexaoController {
     @GetMapping("/editar/{id}")
     public String editarVeiculo(@PathVariable("id") long id, Model model) {
         VeiculoDTO veiculoVelho = veiculoService.findVeiculoById(id);
-        model.addAttribute("veiculo",veiculoVelho);
+        model.addAttribute("veiculoVelho",veiculoVelho);
         return "/alteracao";
     }
 
     @PostMapping("/editar/{id}")
-    public String editarVeiculo(@PathVariable("id") long id, @Valid VeiculoFormUpdate veiculoFormUpdate, BindingResult result, RedirectAttributes attributes) {
-        if (result.hasErrors()) {
-            attributes.addFlashAttribute("mensagem","Deu xabu. Revise suas alterações");
+    public String editarVeiculo(@PathVariable("id") long id,@Valid VeiculoFormUpdate veiculoFormUpdate, BindingResult problem, RedirectAttributes attributes) {
+        if (problem.hasErrors()) {
+            attributes.addFlashAttribute("xabu","Deu Xabu. Revise suas alterações");
             return "redirect:/editar/{id}";
         }
         veiculoFormUpdate.setUpdated(Date.from(Instant.now()));
